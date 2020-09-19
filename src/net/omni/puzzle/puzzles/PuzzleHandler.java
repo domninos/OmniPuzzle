f0be2323f8a01b5c63b93565844e17fba2215ae2
domninos <domninosdev@gmail.com>
2020-09-19 10:37:30 +0800

    Updated .gitignore

--- src/net/omni/puzzle/puzzles/PuzzleHandler.java
+++ src/net/omni/puzzle/puzzles/PuzzleHandler.java
@@ -3,7 +3,6 @@ package net.omni.puzzle.puzzles;
 import net.omni.puzzle.PuzzlePlugin;
 import net.omni.puzzle.util.LocationUtil;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
